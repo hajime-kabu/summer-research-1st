9c57fa567befecbf5dbc1e0ca9a1510355c5a52d
kabu <kabu.kabu.org@gmail.com>
2021-09-01 06:59:32 +0900

    fix: change model to uniform dist

--- exp/model.py
+++ exp/model.py
@@ -5,7 +5,7 @@ from sklearn.dummy import DummyClassifier
 
 class MyModel(object):
     def __init__(self):
-        self.model = DummyClassifier(strategy="most_frequent")
+        self.model = DummyClassifier(strategy="uniform")
 
     def fit(self, X, y):
         self.model.fit(X, y)
