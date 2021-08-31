d2ffada34e5756d0cc49a9b100f8e6364ffea79f
kabu <kabu.kabu.org@gmail.com>
2021-09-01 07:09:20 +0900

    fix: change model to gap markov

--- exp/model.py
+++ exp/model.py
@@ -1,14 +1,29 @@
 import numpy
 import pandas
-from sklearn.dummy import DummyClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
 
 
 class MyModel(object):
     def __init__(self):
-        self.model = DummyClassifier(strategy="uniform")
+        self.nb = MultinomialNB()
+    
+    def _get_dummy_x(self, X):
+        result = pandas.DataFrame()
+
+        for col in X:
+            dummies = pandas.get_dummies(X[col])
+            for dummy_label in dummies:
+                col_name = "{},{}".format(col, dummy_label)
+                result[col_name] = dummies[dummy_label]
+        return result
 
     def fit(self, X, y):
-        self.model.fit(X, y)
+        data = self._get_dummy_x(X)
+
+        self.nb.fit(data, y)
     
     def predict(self, X):
-        return self.model.predict(X)
+        data = self._get_dummy_x(X)
+        return self.nb.predict(data)
